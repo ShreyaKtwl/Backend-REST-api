51d0d9a8adaa4c032586c61805bd10a576e086bf
Your Name <shreyakatuwal@outlook.com>
2020-03-20 14:46:02 +0545

    Project start commit

---
+++ venv1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shreya/Workspace/PycharmProjects/DjangoPractice/profiles-rest-api/venv1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
